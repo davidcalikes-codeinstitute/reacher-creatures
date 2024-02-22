97bfb3052311f3d7d2791ad041b385ea01e90463
David Calikes <david.calikes@codeinstitute.net>
2024-02-22 23:45:04 +0000

    feat: Add generic view/template for home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class HomePage(generic.TemplateView):
+    """
+    Displays hero carousel and CTA to about page"
+    """
+    template_name = 'index.html'
+    
